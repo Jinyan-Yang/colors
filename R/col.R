2f619055eedb88747112faf565ffbdbf060d5e82
JinyanYang <jyyoung@live.com>
2020-12-03 16:29:41 +1100

    v 1.5

--- R/col.R
+++ R/col.R
@@ -1,16 +1,15 @@
 pre.ls <- ls()
 
 #
-col.flower <- c('#5f5856','#dbb232','#906f5a','#988d8d','#000000')
+col.flower <- c('#dbb232','#906f5a','#988d8d','#5f5856','#000000')
 
-col.beach <- c('#3f3342', '#e5ecd5','#a5b5c4','#576f92','#7f7f96')
+col.beach <- c( '#e5ecd5','#a5b5c4','#7f7f96','#576f92','#3f3342')
 
-col.citynight <- c('#2d1916','#5d3a31', '#bf1713', '#bbb3a7','#FFF48A')
+col.citynight <- c('#2d1916','#5d3a31', '#bf1713','#FFF48A','#bbb3a7')
 
-col.auLandscape <- c('#463e1b','#906e47','#abc0df','#a69458','#A9C058')
+col.auLandscape <- c('#463e1b','#A9C058','#a69458','#906e47','#abc0df')
 # c('#96b7dd','#6f9dd5','#453725','#ac8d4c','#bfc3bc')
-
-col.resprout <- c('#1d1d21','#dcb9ab','#4f4b4e','#bb9b59','#a76a58')
+col.resprout <- c('#1d1d21','#4f4b4e','#dcb9ab','#bb9b59','#a76a58')
 
 col.daisy <- c('#222619', '#4b4b4a','#532a2c','#ad3f3e','#c59764')
 
@@ -18,9 +17,9 @@ col.iris <- c('#ECC834','#a5a1af','#855AB4','#1f2509','#839144')
 
 col.beauty <- c('#a9b6ca','#6d9cd4','#a88f6c','#524330','#F4D1D5')
 
-col.bushBySea <- c('#a8b0cd', '#4a3f20','#a69762','#737983','#49689E')
+col.bushBySea <- c('#a69762', '#4a3f20','#a8b0cd','#737983','#49689E')
 
-col.reptile <- c('#430248','#ae54c1','#462aba','#cba0f2','#000000')
+col.reptile <- c('#cba0f2','#ae54c1','#430248','#462aba','#000000')
 
 #
 col.df <- data.frame(flower = col.flower,
