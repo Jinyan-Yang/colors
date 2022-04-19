bd0885fc9a39bc06d3e8c754c976e79aecb8344e
JinyanYang <jyyoung@live.com>
2022-04-19 10:00:17 +1000

    added transprent color

--- R/col.R
+++ R/col.R
@@ -55,6 +55,24 @@ print.all.func <- function(){
 
 }
 
+# print transparent color
+t_col <- function(color, percent = 50, name = NULL) {
+  #      color = color name
+  #    percent = % transparency
+  #       name = an optional name for the color
+
+  ## Get RGB values for named color
+  rgb.val <- col2rgb(color)
+
+  ## Make new color using input color as base and alpha set by transparency
+  t.col <- rgb(rgb.val[1], rgb.val[2], rgb.val[3],
+               max = 255,
+               alpha = (100 - percent) * 255 / 100,
+               names = name)
+
+  ## Save the color
+  invisible(t.col)
+}
 # print.all.func()
 disc = c('Description',
          'This code is a homemade color palette from picture I have taken over the year.',
